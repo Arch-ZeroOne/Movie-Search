2d7779aa7815001dea36a3f41f08b765ba286f27
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-03-01 23:21:22 +0800

    Initialized the project dependencies

---
+++ src/components/Test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Test() {
+  return (
+    <div className="text-white bg-black p-5 rounded-[20px] text-center font-[Jetbrains_Mono] w-[30%]">
+      Test
+    </div>
+  );
+}
+
+export default Test;

22f33d44b51198f2d8e2be4215c7399ae9ec288e
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-03-17 20:41:34 +0800

    Resolved merge conflict problems

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Searchbar from "./Searchbar";
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import TopRated from "./Movie-Types/TopRated";
 import Movies from "./Movie-Types/Movies";
@@ -116,40 +115,6 @@ const App = () => {
         </div>
       </div>
     </BrowserRouter>
-=======
-import MovieCard from "./MovieCard";
-import { useEffect, useState } from "react";
-import axios from "axios";
-const App = () => {
-  const [movies, createMovies] = useState([]);
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      url: "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMGU4MTBlOTAwMDc0ZGI2ODBmNzJjZWNjODI1MTdhMCIsIm5iZiI6MTc0MjA1MDMxOS4xNSwic3ViIjoiNjdkNTk0MGY3OTBkNzg2MzNhMDEzN2U0Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.nKNQgx1xor8E42nVIUGUAfLexUCaCUK13T83LV6oQdY",
-      },
-    };
-    axios
-      .request(options)
-      .then((res) => {
-        createMovies(res.data.results);
-      })
-      .catch((err) => console.error(err));
-  }, []);
-
-  return (
-    <div className="grid grid-cols-1 gap-10">
-      <Searchbar />
-      <div className="grid grid-cols-3 text-white gap-10 font-[Jetbrains_Mono]">
-        {movies.map((movie) => (
-          <MovieCard title={movie.title} backdrop_path={movie.backdrop_path} />
-        ))}
-      </div>
-    </div>
->>>>>>> de19dcb2e690f24fd2a29e3b2ea1b19c718a8b1c
   );
 };
 

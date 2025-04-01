4d2ad7f0d296ff97583f003193953b315e0efcad
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-04-01 09:51:53 +0800

    Modified a little

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -40,45 +40,6 @@ const App = () => {
   const browserPath = useLocation();
   const navigate = useNavigate();
 
-  const handleRouteChange = (current_place) => {
-    let url = "";
-
-    switch (current_place) {
-      case "TV Shows":
-        url = `https://api.themoviedb.org/3/search/tv?query=${value}&include_adult=false&language=en-US&page=1`;
-        break;
-      default:
-        url = `https://api.themoviedb.org/3/search/movie?query=${value}&include_adult=false&language=en-US&page=1`;
-        break;
-    }
-
-    axios
-      .get(url, {
-        headers: {
-          accept: "application/json",
-          Authorization:
-            '"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMGU4MTBlOTAwMDc0ZGI2ODBmNzJjZWNjODI1MTdhMCIsIm5iZiI6MTc0MjA1MDMxOS4xNSwic3ViIjoiNjdkNTk0MGY3OTBkNzg2MzNhMDEzN2U0Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.nKNQgx1xor8E42nVIUGUAfLexUCaCUK13T83LV6oQdY',
-        },
-      })
-      .then((response) => {
-        switch (current_place) {
-          case "Home":
-            setTopRated(response.data.results);
-            break;
-          case "Movies":
-          
-            setMovies(response.data.results);
-            break;
-          case "Upcoming":
-            setUpcoming(response.data.results);
-            break;
-          default:
-            setTvShows(response.data.results);
-            break;
-        }
-      });
-  };
-
   useEffect(() => {
     //* If the path change it sets the location state to the name of the url path
     if (browserPath.pathname === "/") {
@@ -146,7 +107,6 @@ const App = () => {
   useEffect(() => {
     const route = [{ route: browserPath.pathname }];
     localStorage.setItem("Route", JSON.stringify(route));
-    handleRouteChange(browserPath.pathname);
   }, [location]);
 
   return (
